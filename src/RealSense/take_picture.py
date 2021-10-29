67edad129d6090ae4f0d0e31ee3cb8d97a5ec30d
LucasSebille <lsebil21@student.aau.dk>
2021-10-29 14:14:47 +0200

    small change for raspebrry Pi

--- src/RealSense/take_picture.py
+++ src/RealSense/take_picture.py
@@ -1,9 +1,7 @@
-import cv2                                # state of the art computer vision algorithms library
 import numpy as np                        # fundamental package for scientific computing
 import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
-import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
-import open3d as o3d
-print("Environment Ready") 
+import pyrealsense2.pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
+print("Environment Ready")
 
 # Configure depth and color streams
 # Change resolution here
@@ -19,19 +17,18 @@ profile = pipe.start(cfg)
 colorizer = rs.colorizer()
 
 # Skip 5 first frames to give the Auto-Exposure time to adjust
-for x in range(5):
-  pipe.wait_for_frames()
- 
+for x in range(5):pipe.wait_for_frames()
+
 # Store frameset
 frameset = pipe.wait_for_frames()
 color_frame = frameset.get_color_frame()
 depth_frame = frameset.get_depth_frame()
- 
+
 # Cleanup
 pipe.stop()
 print("Frames Captured")
 
-# display 2D-images 
+# display 2D-images
 color = np.asanyarray(color_frame.get_data())
 plt.rcParams["axes.grid"] = False
 colorized_depth = np.asanyarray(colorizer.colorize(depth_frame).get_data())
@@ -42,4 +39,3 @@ plt.subplot(1, 2, 2)
 plt.title('depth image')
 plt.imshow(colorized_depth)
 plt.show()
-
