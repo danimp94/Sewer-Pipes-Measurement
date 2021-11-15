8ae6ec0e1cf32348239eb880c90bde4b3ef4a93a
LucasSebille <lsebil21@student.aau.dk>
2021-11-15 14:24:23 +0100

    active vision + ransac

--- src/RealSense/take_picture.py
+++ src/RealSense/take_picture.py
@@ -4,7 +4,9 @@ import pyrealsense2.pyrealsense2 as rs    # Intel RealSense cross-platform open-
 import open3d as o3d
 import imageio
 import cv2
+import pyransac3d as pyrsc
 print("Environment Ready")
+
 
 # Configure depth and color streams
 # Change resolution here
@@ -13,9 +15,8 @@ cfg = rs.config()
 cfg.enable_stream(rs.stream.depth)
 cfg.enable_stream(rs.stream.color)
 
-
 #Start streaming
-profile = pipe.start(cfg)
+pipe.start(cfg)
 
 # Filter generates color images based on input depth frame
 colorizer = rs.colorizer()
@@ -34,7 +35,12 @@ profile = pipe.get_active_profile()
 # Change the type of stereo vision
 device = profile.get_device()
 depth_sensor = device.query_sensors()[0]
-depth_sensor.set_option(rs.option.emitter_enabled, 1)
+emitter = depth_sensor.get_option(rs.option.emitter_enabled)
+print("emitter = ", emitter)
+set_emitter = 0 #0 for active stereo vision, 1 for passive stereo vision
+depth_sensor.set_option(rs.option.emitter_enabled, set_emitter)
+emitter1 = depth_sensor.get_option(rs.option.emitter_enabled)
+print("new emitter = ", emitter1)
 
 print(profile)
 depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
@@ -44,6 +50,7 @@ print(depth_intrinsics)
 w, h = depth_intrinsics.width, depth_intrinsics.height
 print(w,h)
 
+
 # Cleanup
 pipe.stop()
 print("Frames Captured")
@@ -52,6 +59,7 @@ print("Frames Captured")
 depth_image = np.asanyarray(depth_frame.get_data())
 color_image = np.asanyarray(color_frame.get_data())
 depth_image = cv2.resize(depth_image, (1280, 720))
+color_image = cv2.resize(color_image, (1280, 720))
 
 imageio.imwrite("depth.png", depth_image)
 imageio.imwrite("rgb.png", color_image)
@@ -59,6 +67,7 @@ print("Files saved")
 
 
 print("Create pointcloud...")
+
 color_raw = o3d.io.read_image("rgb.png")
 depth_raw = o3d.io.read_image("depth.png")
 rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
@@ -73,13 +82,35 @@ plt.title('Depth image')
 plt.imshow(rgbd_image.depth)
 plt.show()
 
-p = o3d.camera.PinholeCameraIntrinsic()
+p = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
 p.intrinsic_matrix=[[421.139, 0.0, 426.176], [ 0.0, 421.139, 237.017], [ 0.0, 0.0, 1.0]]
 pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
     rgbd_image,p)
+
 # Flip it, otherwise the pointcloud will be upside down
 pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+# Save the point cloud
+o3d.io.write_point_cloud("cloud.ply", pcd)
+# Get back the point cloud
+pcd_load = o3d.io.read_point_cloud("cloud.ply")
+# Visualize the point cloud
 o3d.visualization.draw_geometries([pcd])
 
-o3d.io.write_point_cloud("cloud.ply", pcd)
+# open3d librairie to use RANSAC for a circular shape
+circ = pyrsc.Circle()
 
+# convert Open3D.o3d.geometry.PointCloud to numpy array (RANSAC needs a numpy array to work)
+xyz_load = np.asarray(pcd_load.points)
+# RANSAC implementation for circular shape detection in point clouds
+center, axis, radius, inliers = circ.fit(xyz_load, thresh=0.1, maxIteration=2)
+
+# Select the inliers and the outliers points
+inlier_cloud = pcd.select_by_index(inliers)
+outlier_cloud = pcd.select_by_index(inliers, invert=True)
+
+# Paint the inliers points in red and the rest in grey
+inlier_cloud.paint_uniform_color([1, 0, 0])
+outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
+
+# Visualize the inlier and outlier point clouds
+o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
