30d0f4e6685360e48456ccce72b8257f08d9be35
danimp94 <danimp94@users.noreply.github.com>
2021-11-21 17:16:33 +0100

    Generating code, first try

---
+++ src/Dynamics/LQR_Model_1_input_grt_rtw/LQR_Model_1_input_types.h
@@ -0,0 +1,35 @@
+/*
+ * LQR_Model_1_input_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "LQR_Model_1_input".
+ *
+ * Model version              : 1.25
+ * Simulink Coder version : 9.5 (R2021a) 14-Nov-2020
+ * C source code generated on : Sun Nov 21 16:01:36 2021
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objective: Debugging
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_LQR_Model_1_input_types_h_
+#define RTW_HEADER_LQR_Model_1_input_types_h_
+#include "rtwtypes.h"
+#include "builtin_typeid_types.h"
+#include "multiword_types.h"
+
+/* Model Code Variants */
+
+/* Parameters (default storage) */
+typedef struct P_LQR_Model_1_input_T_ P_LQR_Model_1_input_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_LQR_Model_1_input_T RT_MODEL_LQR_Model_1_input_T;
+
+#endif                               /* RTW_HEADER_LQR_Model_1_input_types_h_ */
---
+++ src/Dynamics/Matlab_Linear_Model.m
@@ -0,0 +1,126 @@
+%% Fixed Simulation Problem
+clear all, close all, clc
+
+% Constants
+g = 9.81; % Gravity constant
+
+% Box Dimensions
+t1 = 0.2; % height of box 1 (m)
+l1 = 0.3; % lenght of box 1 (m)
+w1 = 0.2; % widht of box 1 (m)
+
+t2 = 0.12; % height of box 2 (m)
+l2 = 0.1; % lenght of box 2 (m)
+w2 = 0.15; % widht of box 2 (m)
+
+% Box Weights
+m1 = 4;   % Mass of box 1 (big box) (kg)
+m2 = 1.4;   % Mass of box 2 (little box) (kg)
+m3 = 0; % Mass of the stick (kg) --> Distributed in m1 and m2
+
+% Rotational Friction (viscosity)
+Br = 2; % Rotational Friction constant
+
+% Parameters needed:
+
+% Variables to control
+theta = 0; % Angle to stabilize the system ---------
+
+% IT SHOULD BE ON f(x)
+l3o = 0.15; % Initial linear actuator lenght (m)
+l3f = l3o + 0.15; % Max lenght of linear actuator (m)
+l2f = (l1 + l2)/2 + l3f + l3o; % Max distance from rotation to c.g box 2(m)                   
+
+% Gravity forces
+F1 = -m1 * g;
+F2 = -m2 * g;
+F3 = -m3 * g;
+
+% Inertia for the axis of rotation
+I1 = (m1 * (t1^2 + l1^2))/12; % Rotational inertia of box 1 [Kg*m^2]
+I2 = (m2 * (t2^2 + l2^2))/12 + (m2 * (l2f)^2) ; % Rotational inertia of box 2 [Kg*m^2]
+ 
+% Inertia constant an the final point of the actuator extension
+I = I1 + I2; % Rotational Inertia
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Simple model (l2c variable but I constant)
+% Matrices Definition
+A = [0 1 ; 0 -Br/I]; 
+B = [0 0 ; F2/I 1];
+B = [0 ;1];
+%B = [B(:,2)];
+
+C = [1 0];
+D = 0;
+ 
+eig(A);
+
+% Check controllability
+controllability =['Check Controllability = ',num2str(rank(ctrb(A,B)))];
+disp(controllability)
+
+% Number of states 
+n = length(A);
+
+% Output matrix
+%C = eye(length(A));
+
+x0 = [pi/8;0]; % Initial conditions
+u0 = [-m2*g*(l2f+l3o-l3f)] % Initial Input
+
+% LQR
+Q = eye(size(C,1))*1;
+%Q(1,1) = 1e-6;
+%Q(2,2) = 1e-6;
+%Q = C'*C;
+R = 1;
+K = lqr(A,B,Q,R);
+
+% Closed loop system
+Ac = [(A-B*K)];
+Bc = [B];
+Cc = [C];
+Dc = [D];
+
+eig(Ac)
+
+sys_cl = ss(Ac,Bc,Cc,Dc);
+
+% Simulate closed-loop system
+figure
+initial(sys_cl,x0)
+legend('LQR_Initial_conditions')
+
+t = 0:0.1:20;
+
+% r =0.2 * ones(size(t));
+% [y,t,x]=lsim(sys_cl,r,t);
+% [AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+% set(get(AX(1),'Ylabel'),'String','cart position (m)')
+% set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)')
+
+figure
+step(sys_cl,t)
+title('Step Response with LQR Control')
+%legend('LQR_step_response')
+
+% MPC ??
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
