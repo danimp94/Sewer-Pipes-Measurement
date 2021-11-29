abad1223a9a4d916715bd99fe976498f31c679c1
Zarinalisa <63066546+Zarinalisa@users.noreply.github.com>
2021-11-29 11:09:13 +0100

    subprocess test

---
+++ src/RealSense/subprocess_test1/subprocess/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    for (int ii = 0; ii < 10; ++ii) {
+        int input;
+        std::cin >> input;
+        std::cout << input * 2 << std::endl;
+        std::cout.flush();
+    }
+}
