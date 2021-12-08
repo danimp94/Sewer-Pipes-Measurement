2ff65bd754f79bf1918ea7170816bc2fe250eea2
LucasSebille <lsebil21@student.aau.dk>
2021-12-08 13:47:59 +0100

    Update PC_848-480_op.py

--- src/realsense-ransac/PC_848-480_op.py
+++ src/realsense-ransac/PC_848-480_op.py
@@ -6,6 +6,9 @@ import imageio
 import cv2
 import pyransac3d as pyrsc                # open3d librairie to use RANSAC for different shapes
 from datetime import datetime as date     # Library use to get the actual date and time
+import subprocess
+from subprocess import Popen, PIPE
+import os
 print("Environment Ready")
 
 
@@ -64,44 +67,11 @@ pcd = o3d.geometry.PointCloud.create_from_depth_image(
 # Flip itthe point cloud, otherwise it will be upside down
 pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
 
-pcd_imp = o3d.geometry.PointCloud.remove_non_finite_points(pcd)
-
-# open3d librairie to use RANSAC for a circular shape
-circ = pyrsc.Circle()
-
-# convert Open3D.o3d.geometry.PointCloud to numpy array (RANSAC needs a numpy array to work)
-xyz = np.asarray(pcd.points)
-# RANSAC implementation for circular shape detection in point clouds
-center, axis, radius, inliers = circ.fit(xyz, thresh=0.03, maxIteration=100)
-print(radius*2000)
-
-# Select the inliers and the outliers points
-inlier_cloud = pcd.select_by_index(inliers)
-outlier_cloud = pcd.select_by_index(inliers, invert=True)
-
-# Paint the inliers points in red and the rest in grey
-inlier_cloud.paint_uniform_color([1, 0, 0])
-outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
-
-# Merge the two point clouds
-# Save the data structure
-inlier_np = np.asarray(inlier_cloud.points)
-outlier_np = np.asarray(outlier_cloud.points)
-# Add the point clouds all together
-rans_np = np.concatenate((inlier_np,outlier_np), axis=0)
-# Get the individuals colors of each point cloud
-inlier_color = np.asarray(inlier_cloud.colors)
-outlier_color = np.asarray(outlier_cloud.colors)
-rans_color = np.concatenate((inlier_color, outlier_color), axis=0)
-# Create a new pointcloud instance and read the pointcloud, color together
-rans = o3d.geometry.PointCloud()
-rans.points = o3d.utility.Vector3dVector(rans_np)
-rans.colors = o3d.utility.Vector3dVector(rans_color)
-
-# Visualize the inlier and outlier point clouds
-o3d.visualization.draw_geometries([rans])
-
 # Save the point cloud
 o3d.io.write_point_cloud("cloud"+timestamp+".ply", pcd)
-# Save the inlier and outlier point cloud
-o3d.io.write_point_cloud("ransac_cloud"+timestamp+".ply", rans)
+# Get back the point cloud
+pcd_load = o3d.io.read_point_cloud("cloud"+timestamp+".ply")
+
+p = subprocess.Popen(['RansacShapeAligner/out/build/x64-Debug/main', '-f', "cloud"+timestamp+".ply"], stdout=PIPE, stdin=PIPE, shell=True) 
+#result = p.stdout.readline().strip()
+#print(result) 
