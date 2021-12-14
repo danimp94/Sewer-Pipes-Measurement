faa424ecd7fa25d1729c325a06847c8525f6eb68
LucasSebille <lsebil21@student.aau.dk>
2021-12-14 12:27:39 +0100

    Update PC_1280-720.py

--- src/realsense-ransac/RansacShapeAligner/out/build/x64-Debug/PC_1280-720.py
+++ src/realsense-ransac/RansacShapeAligner/out/build/x64-Debug/PC_1280-720.py
@@ -15,10 +15,11 @@ actuator = 4 # Broadcom pin 4 (pin 7 on pi)
 
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.cleanup() # cleanup all GPIO
 GPIO.setup(actuator, GPIO.OUT) # LED pin set as output
 
 # 3.3v on pin 7 pi:
-GPIO.output(actuator, GPIO.HIGH)
+GPIO.output(actuator, GPIO.LOW)
 time.sleep(10)
 print("Sleep Over")
 
