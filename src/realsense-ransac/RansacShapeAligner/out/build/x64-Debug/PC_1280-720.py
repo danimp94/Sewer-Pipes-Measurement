2bab245875c720ae413145e0172d9b16fbdf61c1
LucasSebille <lsebil21@student.aau.dk>
2021-12-14 12:21:13 +0100

    Update PC_1280-720.py

--- src/realsense-ransac/RansacShapeAligner/out/build/x64-Debug/PC_1280-720.py
+++ src/realsense-ransac/RansacShapeAligner/out/build/x64-Debug/PC_1280-720.py
@@ -15,7 +15,6 @@ actuator = 4 # Broadcom pin 4 (pin 7 on pi)
 
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-GPIO.cleanup() # cleanup all GPIO
 GPIO.setup(actuator, GPIO.OUT) # LED pin set as output
 
 # 3.3v on pin 7 pi:
